9443fa78f6182b620da2647fe3716a527a7cec55
Noireuu <64626893+SunWithCat@users.noreply.github.com>
2025-07-20 10:34:00 +0800

    Delete lib/config.dart

--- lib/config.dart
+++
@@ -1,6 +0,0 @@
-class AppConfig {
-  static const String githubClientId = 'Iv23liV2kVkoXPYb16UH';
-  static const String githubClientSecret =
-      'a2f640af9ce2d1992dda344e77b0f80d95a91d74';
-  static const String githubCallbackUrl = 'myfluttergithubapp://callback';
-}

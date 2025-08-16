3956a0ae3b6da16550c1a9e26015c281ffa17210
SunWithCat <2770373374@qq.com>
2025-08-16 15:45:16 +0800

    应用退出时关闭Hive Box

--- lib/main.dart
+++ lib/main.dart
@@ -58,8 +58,32 @@ void main() async {
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.detached) {
+      Hive.close(); // 关闭 Hive Box
+    }
+  }
 
   @override
   Widget build(BuildContext context) {

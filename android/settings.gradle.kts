87ab6f343247e1553c941063d2db2ec78f845ef8
SunWithCat <2770373374@qq.com>
2026-03-12 09:46:50 +0800

    feat: Configure Android build system using Gradle Kotlin DSL and update `package_info_plus` to 9.0.0.

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -13,12 +13,15 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
+        maven { url = uri("https://storage.googleapis.com/download.flutter.io") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
     }
 }
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.7.0" apply false
+    id("com.android.application") version "8.9.1" apply false
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
 }
 
